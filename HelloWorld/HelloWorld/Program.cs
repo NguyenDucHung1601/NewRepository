17314b0d716b3e49edaedb9f0f73b70ead9fb5c3
Nguyễn Đức Hưng <48803420+NguyenDucHung1601@users.noreply.github.com>
2021-03-05 15:19:01 +0700

    persional code

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello HaNoi");
             Console.WriteLine("Hello VietNam");
         }
     }
