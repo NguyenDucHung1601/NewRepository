28d3beac344e8c627d15d3222933028db5db3f23
Nguyễn Đức Hưng <48803420+NguyenDucHung1601@users.noreply.github.com>
2021-03-10 11:14:06 +0700

    message

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Nguyen Duc Hung");
+            Console.WriteLine("Hoc vien Ky thuat Quan su");
         }
     }
 }
